bcc7f83e05cd97edb66c8b7b79f2029b891d4561
1less1 <olhava.dom@gmail.com>
2024-12-09 21:42:51 -0600

    I basically have one seasons worth of games added to each team!

---
+++ data_insertion_scripts/insert_base_seasons.sql
@@ -0,0 +1,7 @@
+USE baseball_game_db;
+
+-- Inserting the first 20 seasons of my fantasy baseball league's existence since "2023"!
+INSERT INTO `season` (`Year`) VALUES
+(2023);
+
+SELECT * FROM Season;
